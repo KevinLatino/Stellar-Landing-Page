da211064356753ecb8c72746405e6090cb1c97da
KevinLatino <latinokevin9@gmail.com>
2024-09-12 17:58:04 -0600

    new tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,33 @@ const config: Config = {
       },
     },
     extend: {
+
+      fontFamily: {
+        'raleway': ['Raleway', 'sans-serif'],
+      },
+
+      colors: {
+        //Stellar colors (for navbar, etc)
+        "stellar-icon-color": "#BFBFBF",
+        "stellar-blue": "#0B103D",
+        "stellar-white": "#EDF0F7",
+        "stellar-grey": "#565555",
+        "stellar-text": "000000a3",
+
+        //strong colors
+        "strong-blue": "#14162E",
+        "strong-red": "#Eb2828",
+        'purple-strong': '#763DF2',
+
+        //light colors
+        "light-White": "#EDF0F7",
+        "light-yellow": "#F9B742",
+        "light-blue": "#4461F2",
+        "light-green": "#48BC5E",
+        "light-red": "#EF4545",
+        'purple-light': '#C364FF',
+      },
+      
       animation: {
         "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
         slide: "slide var(--speed) ease-in-out infinite alternate",
