f0ace3cc90dbe91a6ffdbb35ed1086a0873b7291
KevinLatino <latinokevin9@gmail.com>
2024-09-12 17:57:38 -0600

    adding new features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
-import Image from "next/image";
+import ShimmerButton from "@/components/magicui/shimmer-button";
 
 export default function Home() {
   return (
-    <h1></h1>
+
+    <div className="flex w-full mt-24 justify-evenly">
+      <div className="flex w-[37%] flex-col gap-7">
+        <h1 className="text-strong-blue text-3xl text-center">
+          Con <strong>Stellar</strong> ¡Nunca había sido tan sencillo que los estudiantes controlen su productividad!
+        </h1>
+        <div className="flex items-center justify-center">
+          <ShimmerButton className="shadow-2xl">
+            <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+              Shimmer Button
+            </span>
+          </ShimmerButton>
+        </div>
+      </div>
+      <div className="flex w-1/2">
+        {/* future computer */}
+      </div>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,41 +1,50 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: "",
   theme: {
-    extend: {
-      
-      fontFamily: {
-        'raleway': ['Raleway', 'sans-serif'],
-      },
-
-      colors: {
-        //Stellar colors (for navbar, etc)
-        "stellar-icon-color": "#BFBFBF",
-        "stellar-blue": "#0B103D",
-        "stellar-white": "#EDF0F7",
-        "stellar-grey": "#565555",
-        "stellar-text": "000000a3",
-
-        //strong colors
-        "strong-blue": "#14162E",
-        "strong-red": "#Eb2828",
-        'purple-strong': '#763DF2',
-
-        //light colors
-        "light-White": "#EDF0F7",
-        "light-yellow": "#F9B742",
-        "light-blue": "#4461F2",
-        "light-green": "#48BC5E",
-        "light-red": "#EF4545",
-        'purple-light': '#C364FF',
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
     },
-  },
-  plugins: [],
-};
-export default config;
+    extend: {
+      animation: {
+        "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
+        slide: "slide var(--speed) ease-in-out infinite alternate",
+      },
+      keyframes: {
+        "spin-around": {
+          "0%": {
+            transform: "translateZ(0) rotate(0)",
+          },
+          "15%, 35%": {
+            transform: "translateZ(0) rotate(90deg)",
+          },
+          "65%, 85%": {
+            transform: "translateZ(0) rotate(270deg)",
+          },
+          "100%": {
+            transform: "translateZ(0) rotate(360deg)",
+          },
+        },
+        slide: {
+          to: {
+            transform: "translate(calc(100cqw - 100%), 0)",
+          },
+        },
+      },
+    },
+  }
+}
+
+export default config
