d156c80ee720891847852a211c92c48f6cee463e
KevinLatino <latinokevin9@gmail.com>
2024-09-12 16:42:24 -0600

    basic navbar is now finished

--- src/components/NavBarComponent.tsx
+++ src/components/NavBarComponent.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'; // Importa el componente de Next.js
 import StellarLogo from '@/assets/Stellar-NavBar.svg';
 
 export default function NavBarComponent() {
@@ -8,9 +9,15 @@ export default function NavBarComponent() {
     ];
 
     return (
-        <nav className="p-8 flex justify-end">
-            <img src={StellarLogo} />
-            <ul className="flex gap-9">
+        <nav className="flex justify-between items-center">
+            <Image 
+                src={StellarLogo} 
+                alt="Stellar Logo" 
+                width={290} 
+                height={40} 
+                className="p-8" 
+            />
+            <ul className="flex gap-8 p-8">
                 {navBarItems.map((item, index) => (
                     <li key={index} className="text-black font-raleway text-[20px] font-medium cursor-pointer">
                         {item}
