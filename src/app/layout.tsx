5fe2a5bf43dcb576e396160cc4d85fcb86a70e67
santiago villarreal arley <115122095+Villarley@users.noreply.github.com>
2024-09-15 00:36:01 -0600

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,17 @@ import type { Metadata } from "next";
 import NavBarComponent from "@/components/NavBarComponent";
 import FooterComponent from "@/components/FooterComponent";
 import "./globals.css";
-import Stellar from "@/assets/StellarLogo-Over-Black.svg"
+import Stellar from "./favicon.ico"
 
 export const metadata: Metadata = {
   title: "Stellar",
   description: "This is the landing page of Stellar",
-  icons: Stellar,
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <link rel="icon" href={Stellar.src} />
       <body className="flex flex-col min-h-screen">
         <header id="hero" className="mb-[10rem] ">
           <NavBarComponent />
